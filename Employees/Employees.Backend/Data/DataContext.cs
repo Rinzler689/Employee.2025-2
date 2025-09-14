b62ea5b9900fbca61a321db52148437c907c78a2
Rinzler-PC <Jaramilok@gmail.com>
2025-09-13 19:09:36 -0500

    Add Employees entity and Database

---
+++ Employees/Employees.Backend/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Employees.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
---
+++ Employees/Employees.Shared/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Shared.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nombre")]
+    [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Apellido")]
+    [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string LastName { get; set; } = null!;
+
+    public bool? IsActive { get; set; }
+
+    [Display(Name = "Fecha de Contratación")]
+    [DataType(DataType.Date)]
+    public DateTime? HireDate { get; set; }
+
+    [Display(Name = "Salario")]
+    [Range(1000000, double.MaxValue, ErrorMessage = "El campo {0} debe ser mínimo de $1,000,000.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public decimal Salary { get; set; }
+}
